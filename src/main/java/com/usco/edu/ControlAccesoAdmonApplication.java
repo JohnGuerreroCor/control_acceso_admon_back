46293f047d0cdcee0ef01a8e0ae03275f9e21961
JohnGuerreroCor <john.guerrero@usco.edu.co>
2024-01-16 10:41:41 -0500

    Ajustes denominacion proyecto

--- src/main/java/com/usco/edu/ControlAccesoAdmonApplication.java
+++ src/main/java/com/usco/edu/ControlAccesoAdmonApplication.java
@@ -7,10 +7,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
-public class PuestoVigilanciaAdministradorApplication {
+public class ControlAccesoAdmonApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PuestoVigilanciaAdministradorApplication.class, args);
+		SpringApplication.run(ControlAccesoAdmonApplication.class, args);
 	}
 	
 
--- src/main/java/com/usco/edu/ServletInitializer.java
+++ src/main/java/com/usco/edu/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(PuestoVigilanciaAdministradorApplication.class);
+		return application.sources(ControlAccesoAdmonApplication.class);
 	}
 
 }
